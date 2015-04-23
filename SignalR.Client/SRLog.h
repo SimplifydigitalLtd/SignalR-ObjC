889420f8b20ec836bfc227bf49f5b5233801d148
Shams Ahmed <shamsahmed@me.com>
2015-04-23 22:58:26 +0100

    fixed logs

--- SignalR.Client/SRLog.h
+++ SignalR.Client/SRLog.h
@@ -45,12 +45,10 @@
 #define LOG_WEBSOCKETS          (ddLogLevel & LOG_LEVEL_WEBSOCKETS  )
 #define LOG_AUTOTRANSPORT       (ddLogLevel & LOG_FLAG_AUTOTRANSPORT )
 
-typedef int DDLogLevel;
-
 #if SRLogEnabled == 1
-static DDLogLevel ddLogLevel = LOG_LEVEL_AUTOTRANSPORT;
+static int ddLogLevel = LOG_LEVEL_AUTOTRANSPORT;
 #else
-static DDLogLevel ddLogLevel = LOG_LEVEL_OFF;
+static int ddLogLevel = LOG_LEVEL_OFF;
 #endif
 
 #define COCOA_LUMBER_JACK 0
